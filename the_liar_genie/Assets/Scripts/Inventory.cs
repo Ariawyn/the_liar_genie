097d23adefab42ba710465c6fc8e6e877c9ffed7
hudsonmiears <hudsonmiears@gmail.com>
2019-08-30 16:22:20 +0100

    Picking up, throwing objects with dotween animations. f to throw, p to pickup

---
+++ the_liar_genie/Assets/Scripts/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Inventory : MonoBehaviour
+{
+    public Pickupable heldObject;
+    public Transform objectHolder;
+
+    public float minXThrow, maxXThrow, minZThrow, maxZthrow;
+
+    public float groundYPos;
+
+    public float throwPower;
+
+    public int throwNumJumps;
+
+    public float throwDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objectHolder != null && objectHolder.childCount > 0) {
+            heldObject = objectHolder.GetComponentInChildren<Pickupable>();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            ThrowObject();
+    }
+
+    public void HoldObject(Pickupable toHold) {
+        if (heldObject != null)
+        {
+            ThrowObject();
+        }
+
+        heldObject = toHold;
+        heldObject.transform.parent = objectHolder;
+        heldObject.transform.DOLocalJump(Vector3.zero, throwPower, 1, throwDuration);
+        //heldObject.transform.localPosition = Vector3.zero;
+        heldObject.currentInventoryHeld = this;
+    }
+
+    public void ReleaseObjectToOtherInventory(Inventory other) {
+        heldObject.currentInventoryHeld = null;
+        other.HoldObject(heldObject);
+        this.heldObject = null;
+    }
+
+    public void ThrowObject() {
+        heldObject.transform.parent = null;
+
+        Vector3 throwEnd = new Vector3(objectHolder.transform.position.x + Random.Range(minXThrow, maxXThrow), groundYPos, objectHolder.transform.position.z+Random.Range(minZThrow, maxZthrow));
+
+        heldObject.transform.DOJump(throwEnd, throwPower, throwNumJumps, throwDuration);
+        heldObject.currentInventoryHeld = null;
+        this.heldObject = null;
+    }
+}
---
+++ the_liar_genie/Assets/Scripts/Pickupable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Pickupable : MonoBehaviour
+{
+    SphereCollider sphereCollider;
+
+    Inventory currentInventoryUnheld;
+    public Inventory currentInventoryHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereCollider = this.GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (currentInventoryUnheld != null) {
+                currentInventoryUnheld.HoldObject(this);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Inventory>() != null) {
+            this.currentInventoryUnheld = other.GetComponent<Inventory>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Inventory>() != null && other.GetComponent<Inventory>() == this.currentInventoryUnheld)
+        {
+            this.currentInventoryUnheld = null;
+        }
+    }
+}
