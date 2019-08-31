980fa928f2c17a828fe8eb7c28b98a1ce9c2f8b0
Aria <ariagudlaug@gmail.com>
2019-08-31 05:12:14 +0000

    When applied, this commit adds a whole ass dialogue system, just need to actually implement the speech bubbles themselves and their behaviours with their controllers and everything should be fine? Also testing. Testing might need to happen after next commit then.

---
+++ the_liar_genie/Assets/Scripts/Systems/Dialogue/DialogueResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class DialogueResponse 
+    {
+        public string response_text;
+        public int next_dialogue_line_index;
+    }
+}
---
+++ the_liar_genie/Assets/Scripts/Systems/Dialogue/SpeechBubbleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class SpeechBubbleController : MonoBehaviour
+    {
+        public bool is_displaying = false;
+        public int current_dialogue_response_index = -1;
+
+        public void DisplayDialogueLine(DialogueLine line)
+        {
+            is_displaying = true;
+        }
+
+        public void DisplayDialogueResponses(DialogueResponse[] responses)
+        {
+            is_displaying = true;
+            current_dialogue_response_index = 0;
+        }
+
+        public void ClearSpeechBubble()
+        {
+            is_displaying = false;
+            current_dialogue_response_index = -1;
+        }
+    }
+}
