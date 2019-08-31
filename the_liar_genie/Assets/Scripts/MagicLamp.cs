3cd6836a0c6e519e043bfefa8e967011ee8f5820
hudsonmiears <hudsonmiears@gmail.com>
2019-08-31 18:15:10 +0100

    lamp and pedestal added, script on lamp. genie added but no dialogue.

---
+++ the_liar_genie/Assets/Scripts/MagicLamp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Pickupable))]
+public class MagicLamp : MonoBehaviour
+{
+
+    Pickupable pickupable;
+    Inventory heldInventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupable = GetComponent<Pickupable>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        heldInventory = pickupable.currentInventoryHeld;
+
+        if (heldInventory.gameObject.CompareTag("Pedestal")) {
+            //TODO: Win the game
+        }
+    }
+}
